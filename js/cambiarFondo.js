0953a2de35dba815ada0c4c275580bb4985fbca0
diego santellan <diego.santellan2001@gmail.com>
2023-12-05 15:08:59 -0300

    Segundo avance, agregado del contenido principal y estilo background

--- js/cambiarFondo.js
+++ js/cambiarFondo.js
@@ -1,16 +1,28 @@
 function toggleFondo() {
     // Obtener el elemento body
     var body = document.body;
-    let nav = document.getElementById('nav')
+    let nav = document.getElementById('nav');
+    let aptitudes = document.getElementById('aptitudes');
+    let services = document.getElementById('services');
+    let contact = document.getElementById('contact');
+    let uno = document.getElementById('uno');
+    let dos = document.getElementById('dos'); 
+
     // Alternar la clase para cambiar el fondo
     body.classList.toggle('fondo-oscuro');
     nav.classList.toggle('fondo-oscuro');
-    // // Obtener todas las etiquetas 'a' en el documento
-    // var links = document.getElementsByTagName('a');
+    services.classList.toggle('fondo-oscuro');
+    aptitudes.classList.toggle('fondo-oscuro');
+    contact.classList.toggle('fondo-oscuro');
+    dos.classList.toggle('fondo-oscuro');
+    uno.classList.toggle('fondo-oscuro');
 
-    // // Iterar sobre las etiquetas 'a' y alternar la clase para cambiar el fondo
-    // for (var i = 0; i < links.length; i++) {
-    //     links[i].classList.toggle('fondo-oscuro');
-    // }
+    // Obtener todas las etiquetas 'a' en el documento
+    var links = document.getElementsByTagName('li');
+
+    // Iterar sobre las etiquetas 'a' y alternar la clase para cambiar el fondo
+    for (var i = 0; i < links.length; i++) {
+        links[i].classList.toggle('fondo-oscuro');
+    }
 
 }
