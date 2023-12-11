de4a6c15e8ecc5589023517a2232e70d21b8b211
diego santellan <diego.santellan2001@gmail.com>
2023-12-11 09:53:14 -0300

    agregado de section combos y descarga de CV

--- js/cambiarFondo.js
+++ js/cambiarFondo.js
@@ -1,34 +1,40 @@
-function toggleFondo() {
+async function toggleFondo() {
     // Obtener el elemento body
     var body = document.body;
-    let nav = document.getElementById('nav');
-    let aptitudes = document.getElementById('aptitudes');
-    let services = document.getElementById('services');
-    let contact = document.getElementById('contact');
-    let uno = document.getElementById('uno');
-    let dos = document.getElementById('dos'); 
-    let piloto = document.getElementById('piloto'); 
-    let ivan = document.getElementById('ivan'); 
-    let btn = document.getElementById('btn'); 
 
-    // Alternar la clase para cambiar el fondo
-    body.classList.toggle('fondo-oscuro');
-    nav.classList.toggle('fondo-oscuro');
-    services.classList.toggle('fondo-oscuro');
-    aptitudes.classList.toggle('fondo-oscuro');
-    contact.classList.toggle('fondo-oscuro');
-    uno.classList.toggle('fondo-oscuro');
-    dos.classList.toggle('fondo-oscuro');
-    piloto.classList.toggle('fondo-oscuro');
-    ivan.classList.toggle('fondo-oscuro');
-    btn.classList.toggle('fondo-oscuro');
+    // Obtener IDs de elementos
+    let elementIds = ['nav', 'aptitudes', 'services', 'contact', 'uno', 'dos', 'piloto', 'ivan', 'btn', 'combo', 'comboDos', 'comboTres', 'redes'];
 
-    // Obtener todas las etiquetas 'a' en el documento
-    var links = document.getElementsByTagName('li');
-
-    // Iterar sobre las etiquetas 'a' y alternar la clase para cambiar el fondo
-    for (var i = 0; i < links.length; i++) {
-        links[i].classList.toggle('fondo-oscuro');
+    // Función para obtener un elemento de forma asíncrona
+    async function getElementAsync(elementId) {
+        return new Promise((resolve) => {
+            let element = document.getElementById(elementId);
+            resolve(element);
+        });
     }
 
+    // Función para alternar la clase de un elemento de forma asíncrona
+    async function toggleClassAsync(element, className) {
+        if (element) {
+            element.classList.toggle(className);
+        }
+    }
+
+    // Realizar operaciones de manera asíncrona
+    await toggleClassAsync(body, 'fondo-oscuro');
+
+    for (let elementId of elementIds) {
+        let element = await getElementAsync(elementId);
+        await toggleClassAsync(element, 'fondo-oscuro');
+    }
+
+        // Obtener todas las etiquetas 'a' en el documento
+        var links = document.getElementsByTagName('li');
+
+        // Iterar sobre las etiquetas 'a' y alternar la clase para cambiar el fondo
+        for (var i = 0; i < links.length; i++) {
+            links[i].classList.toggle('fondo-oscuro');
+        }
 }
+// Llamar a la función toggleFondo
+// toggleFondo();
